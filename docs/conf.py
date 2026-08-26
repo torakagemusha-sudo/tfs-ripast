88886da1a4e8bbe8fa2b2b77dc8ba43b3a748bf0
Thomas Helm <torakagemusha@gmail.com>
2026-08-26 11:02:08 +0800

    chore: bump package metadata, CI, docs, and 0.1.1 fixtures to 0.45.1

--- docs/conf.py
+++ docs/conf.py
@@ -1,13 +1,13 @@
 project = "TFS Ripast"
 copyright = "2026, Torafirma Systems"
 author = "Torafirma Systems"
-release = "0.1.0"
+release = "0.1.1"
 
 extensions = ["myst_parser"]
 source_suffix = {".md": "markdown"}
 master_doc = "index"
 templates_path = []
-exclude_patterns = ["_build"]
+exclude_patterns = ["_build", "superpowers", "superpowers/**"]
 myst_heading_anchors = 3
 html_theme = "alabaster"
 html_title = "TFS Ripast documentation"
